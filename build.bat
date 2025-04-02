caa7efeb0ceba72bbd116f8f62e7732680d6b339
NotHammer <107131733+NotHammer043@users.noreply.github.com>
2025-04-02 11:06:27 +0800

    oops

--- build.bat
+++ build.bat
@@ -1,37 +1,23 @@
-name: Build Windows Executable
+@ECHO OFF
+:MENU
+ECHO.
+ECHO Select an option:
+ECHO 1 - nanoMIDIPlayer
+ECHO 2 - Bootstrapper
+SET /P option=Which to build: 
 
-on: [push]
+IF "%option%"=="1" (
+    ECHO building nanoMIDIPlayer...
+    pyinstaller --onefile --icon="./assets/icon.png" --hidden-import=mido.backends.rtmidi --noconsole nanoMIDIPlayer.py
+    GOTO END
+) ELSE IF "%option%"=="2" (
+    ECHO building Bootstrapper...
+    pyinstaller --onefile --icon="./assets/icon.png" --noconsole bootstrapper.py
+    GOTO END
+) ELSE (
+    ECHO Invalid option. Please select 1 or 2.
+    GOTO MENU
+)
 
-jobs:
-  build:
-    runs-on: windows-latest  # Use Windows runner
-
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v3
-
-      - name: Set up Python
-        uses: actions/setup-python@v4
-        with:
-          python-version: '3.10'
-          architecture: 'x64'
-
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install -r requirements.txt
-
-      - name: Build executable
-        uses: Martin005/pyinstaller-action@v1
-        with:
-          spec: nanoMIDIPlayer.py
-          options: '--onefile, --icon=./assets/icon.png, --hidden-import=mido.backends.rtmidi, --noconsole'
-          python_ver: '3.10'
-          python_arch: 'x64'
-          upload_exe_with_name: 'nanoMIDIPlayer_executable'
-
-      - name: Upload executable
-        uses: actions/upload-artifact@v3
-        with:
-          name: nanoMIDIPlayer.exe
-          path: ./dist/nanoMIDIPlayer.exe
+:END
+PAUSE
